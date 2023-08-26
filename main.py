355ae4bf1e9ec1db632f76b524295d1f611362ca
Woo Hyeon (Luke) Her <104393912+365LIT@users.noreply.github.com>
2023-08-26 19:58:53 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,65 @@
+import os
+import tempfile
+from dotenv import load_dotenv
+from langchain.chains import RetrievalQA
+from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import Chroma
+from langchain.embeddings import OpenAIEmbeddings
+import streamlit as st
+import sys
+__import__('pysqlite3')
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
+load_dotenv()
+
+# Title
+st.title("ChatPDF")
+st.write("---")
+
+# File Upload
+uploaded_file = st.file_uploader("Upload a PDF file", type=['pdf'])
+st.write("---")
+
+
+def pdf_to_document(uploaded_file):
+    temp_dir = tempfile.TemporaryDirectory()
+    temp_filepath = os.path.join(temp_dir.name, uploaded_file.name)
+    with open(temp_filepath, "wb") as f:
+        f.write(uploaded_file.getvalue())
+    loader = PyPDFLoader(temp_filepath)
+    pages = loader.load_and_split()
+    return pages
+
+
+# What happens when uploading is completed
+if uploaded_file is not None:
+    pages = pdf_to_document(uploaded_file)
+
+    # Split
+    text_splitter = RecursiveCharacterTextSplitter(
+        # Set a really small chunk size, just to show.
+        chunk_size=300,
+        chunk_overlap=20,
+        length_function=len,
+        is_separator_regex=False,
+    )
+    texts = text_splitter.split_documents(pages)
+
+    # Embedding
+    embeddings_model = OpenAIEmbeddings()
+
+    # Load it into Chroma
+    db = Chroma.from_documents(texts, embeddings_model)
+
+    # Question
+    st.header("Asking questions to PDF!!")
+    question = st.text_input('Write your question')
+    if st.button('Ask your question'):
+        with st.spinner('Coming up with the answer...'):
+            llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+            qa_chain = RetrievalQA.from_chain_type(
+                llm, retriever=db.as_retriever())
+            result = qa_chain({"query": question})
+            st.write(result["result"])
