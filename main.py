123daab681f078c3b64e0e4045db574a6809aee4
Woo Hyeon (Luke) Her <104393912+365LIT@users.noreply.github.com>
2023-08-26 20:22:26 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import os
 import tempfile
 from dotenv import load_dotenv
@@ -8,7 +9,8 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Chroma
 from langchain.embeddings import OpenAIEmbeddings
 import streamlit as st
-import sys
+from streamlit_extras.buy_me_a_coffee import button
+button(username="Woo Hyeon Her", floating=True, width=221)
 __import__('pysqlite3')
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
@@ -17,6 +19,9 @@ load_dotenv()
 # Title
 st.title("ChatPDF")
 st.write("---")
+
+# Open AI KEY
+openai_key = st.text_input('OPEN_AI_API_KEY', type="password")
 
 # File Upload
 uploaded_file = st.file_uploader("Upload a PDF file", type=['pdf'])
@@ -48,7 +53,7 @@ if uploaded_file is not None:
     texts = text_splitter.split_documents(pages)
 
     # Embedding
-    embeddings_model = OpenAIEmbeddings()
+    embeddings_model = OpenAIEmbeddings(openai_api_key=openai_key)
 
     # Load it into Chroma
     db = Chroma.from_documents(texts, embeddings_model)
@@ -58,7 +63,8 @@ if uploaded_file is not None:
     question = st.text_input('Write your question')
     if st.button('Ask your question'):
         with st.spinner('Coming up with the answer...'):
-            llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+            llm = ChatOpenAI(model_name="gpt-3.5-turbo",
+                             temperature=0, openai_api_key=openai_key)
             qa_chain = RetrievalQA.from_chain_type(
                 llm, retriever=db.as_retriever())
             result = qa_chain({"query": question})
